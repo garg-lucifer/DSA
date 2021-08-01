e7e1eb819c4b932f7c16118c8d224f95a8a936ac
Naman Garg <86689556+garg-lucifer@users.noreply.github.com>
2021-08-01 19:11:57 +0530

    Update reverseanarray.java

--- Arrays/reverseanarray.java
+++ Arrays/reverseanarray.java
@@ -1,1 +1,16 @@
-
+class Reverse
+{
+    public static String reverseWord(String input)
+    {
+        int n = input.length();
+        
+        byte[] strAsByteArray = input.getBytes();
+ 
+        byte[] result = new byte[n];
+ 
+        for (int i = 0; i < n; i++)
+            result[i] = strAsByteArray[n - i - 1];
+ 
+        return new String(result);
+    }
+}
